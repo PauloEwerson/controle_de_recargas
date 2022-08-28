dbb13b8e4dd8607401d8cf2ab310c00f74f1ef67
Ewerson Martins <ewersonmrt@gmail.com>
2022-08-28 18:25:13 -0300

    Cria o modelo 'BlogPost' em 'src/database/models/blogPost.js'

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -1,3 +1,7 @@
+/** JSDoc
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
 module.exports = (sequelize, DataTypes) => {
   const createCategory = sequelize.define('Category', {
     id : {
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,3 +1,7 @@
+/** JSDoc
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
 module.exports = (sequelize, DataTypes) => {
   const createUser = sequelize.define('User', {
     id : {
