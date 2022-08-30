ebd86d55fbe84a5d288f85493dd6a24628512045
Ewerson Martins <ewersonmrt@gmail.com>
2022-08-30 02:10:45 -0300

    Validada que não é possível cadastrar usuario com o campo  com formato inválido

--- src/middlewares/mdUser.js
+++ src/middlewares/mdUser.js
@@ -1,13 +1,20 @@
-const joi = require('joi');
+const Joi = require('joi');
 
-const displayName = joi.string().min(8).required();
+const joiDisplayName = Joi.string().min(8).required();
+const joiEmail = Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } });
 
 const mdUser = (req, res, next) => {
   const userData = req.body;
-  const { error } = displayName.validate(userData.displayName);
-  if (error) {
+  const validDisplayName = joiDisplayName.validate(userData.displayName);
+  if (validDisplayName.error) {
  return res.status(400).json({
     message: '"displayName" length must be at least 8 characters long',
+  }); 
+}
+  const validEmail = joiEmail.validate(userData.email);
+  if (validEmail.error) {
+ return res.status(400).json({
+    message: '"email" must be a valid email',
   }); 
 }
   next();
