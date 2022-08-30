028a0d42efd48e13699407136500eec9ff78a1b1
Ewerson Martins <ewersonmrt@gmail.com>
2022-08-30 02:36:00 -0300

    Valida que é possível cadastrar uma pessoa usuária com sucesso

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,21 @@
+const jwt = require('jsonwebtoken');
 const userService = require('../services/userService');
 
+const { JWT_SECRET } = process.env;
 const ERROR_MESSAGE = 'Server error';
 
 const createUser = async (req, res) => {
   try {
     const userData = req.body;
     const resultUser = await userService.createUser(userData);
-    
+
     if (resultUser.message) {
       return res.status(409).json(resultUser);
     }
 
-    return res.status(200).json(resultUser);
+    const token = jwt.sign(userData.email, JWT_SECRET);
+
+    return res.status(201).json({ token });
   } catch (error) { 
     console.log(error);
     return res.status(500).json(ERROR_MESSAGE);
