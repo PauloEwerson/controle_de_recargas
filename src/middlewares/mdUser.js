01666805e5ddcf8c1bc29564a2f48cb5bdf17c2f
Ewerson Martins <ewersonmrt@gmail.com>
2022-08-30 01:48:52 -0300

    Validada que não é possível cadastrar usuario com o campo  menor que 8 caracteres com JOI

--- src/middlewares/mdUser.js
+++ src/middlewares/mdUser.js
@@ -1,13 +1,15 @@
+const joi = require('joi');
+
+const displayName = joi.string().min(8).required();
+
 const mdUser = (req, res, next) => {
   const userData = req.body;
-
-  if (userData.displayName.length < 8) {
-    // displayName devidamente preenchido com 8 caracteres ou mais
-    return res.status(400).json({
-      message: '"displayName" length must be at least 8 characters long',
-    });
-  }
-
+  const { error } = displayName.validate(userData.displayName);
+  if (error) {
+ return res.status(400).json({
+    message: '"displayName" length must be at least 8 characters long',
+  }); 
+}
   next();
 };
 
