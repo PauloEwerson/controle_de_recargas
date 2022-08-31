093f38537fd4dc5ca6ff75304d6481069351747b
Ewerson Martins <ewersonmrt@gmail.com>
2022-08-31 03:19:26 -0300

    Define método de transação 'Unmanaged transactions' na criação do BlogPost e PostCategory, evitando popular o banco de dados de forma inconsistent

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,8 +1,13 @@
+const Sequelize = require('sequelize');
 const {
   Category,
   BlogPost,
   PostCategory,
 } = require('../database/models');
+
+const config = require('../database/config/config');
+
+const sequelize = new Sequelize(config.development);
 
 const checkUserLogged = require('../helpers/checkUserLogged');
 
@@ -18,13 +23,21 @@ const createPost = async (dataPost, authorization) => {
   const { title, content, categoryIds } = dataPost;
   const dataToken = await checkUserLogged(authorization);
 
+  // inicia a transação
+  const t = await sequelize.transaction();
+
   const { dataValues } = await BlogPost.create(
     { title, content, userId: dataToken.id },
+    { transaction: t },
   );
 
   await PostCategory.bulkCreate(
     categoryIds.map((id) => ({ postId: dataValues.id, categoryId: id })),
+    { transaction: t },
   );
+
+  // finaliza a transação
+  await t.commit();
 
   return dataValues;
 };
